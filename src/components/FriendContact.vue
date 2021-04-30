acdc91025f6b0e4407036170b69d0737de17bc53
Przemek <przemek16391@wp.pl>
2021-04-30 17:48:32 +0200

    Working component in CLI

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+    <li>
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails">{{detailsAreVisible ? 'Hide':'Show'}} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{friend.phone}}</li>
+            <li><strong>Email:</strong>{{friend.email}}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            detailsAreVisible:false,
+            friend:{
+                id:"manuel",
+                name:"Manuel Lopez",
+                phone:"3423423423423423",
+                email:"manuel@gmail.com"
+            }
+        }
+    },
+    methods:{
+        toggleDetails(){
+            this.detailsAreVisible=!this.detailsAreVisible;
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
